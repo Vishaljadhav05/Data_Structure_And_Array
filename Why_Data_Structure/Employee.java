929102620ac658a28ead55319b681509600516c7
Vishal <vishaljadhav.vj8827@gmail.com>
2026-01-30 19:54:53 +0530

    Second commit - Why Data Structure

---
+++ Why_Data_Structure/Employee.java
@@ -0,0 +1,35 @@
+public class Employee {
+    
+    public static void main(String[] args) {
+
+        //NEED of Array
+        //IF we have to use multiple element of same type than we use array
+        int empId1 = 10;
+        int empId2 = 20;
+        int empId3 = 20;
+        int empId4 = 20;
+        int empId5 = 20;
+        int empId6 = 20;
+        int empId7 = 20;
+        int empId8 = 20;
+        int empId9 = 20;
+        int empId10 = 20;
+        int empId11 = 20;
+        System.out.println(empId1);
+        System.out.println(empId2);
+        System.out.println(empId3);
+        System.out.println(empId4);
+        System.out.println(empId5);
+        System.out.println(empId6);
+        System.out.println(empId7);
+        System.out.println(empId8);
+        System.out.println(empId9);
+        System.out.println(empId10);
+        System.out.println(empId11);
+
+
+
+
+
+    }
+}
